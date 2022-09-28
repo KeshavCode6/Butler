d1751daf0070c981907767d2026ecf1972ca55f4
Kaysav Shaw <105513707+QentangifyCodes@users.noreply.github.com>
2022-09-28 16:17:16 -0400

    Update 1
    Moved project to github. Completed rickroll feature

---
+++ Butler.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default() # THANK YOU PAUL
+bot = commands.Bot(command_prefix='.', intents=intents, help_command=None)
+TOKEN =  "MTAyNDcwMjAxNTI2NTMyMDk3MA.GazAOi.Stw526c2dHuGFDxCWQBbqhuAKaQdNTqzHLjG0A"
+
+
+# Basically looks for a specified user, joins the vc they are in, and rickrolls them
+@ bot.command()
+async def rickroll(ctx, member=None):
+    await ctx.message.delete()
+
+    if member==None:
+        await ctx.send("Provide a user to rick roll")
+        return
+
+    if "<" in member:
+        member = member.replace("<", "")
+        member = member.replace(">", "")
+        member = member.replace("@", "")
+        id = int(member)
+    else:
+        await ctx.send("Provide the user with a ping (e.x @User123)")
+        return
+
+    for channel in ctx.guild.voice_channels:    
+        members = channel.members
+
+        for user in members:
+            if user.id == id:
+                voice = await channel.connect()
+                voice.play(discord.FFmpegPCMAudio(executable=r"C:\Users\kesha\PythonProjects\ffmpeg.exe", source="Rickroll.mp3"))
+                return
+
+    await ctx.send(f"It seems that user is not in a voice channel right now")
+@ bot.command()
+async def disconnect(ctx):
+    await ctx.message.delete()
+    for b in bot.voice_clients:
+        await b.disconnect()
+        return
+
+    await ctx.send(f"I am not in a voice channel currently.")
+# This messages prints as soon as the bot has logged on
+@ bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+bot.run(TOKEN)
