81dc368ef5e0283a2a98ab936a29a65d8184c73f
Kaysav Shaw <105513707+QentangifyCodes@users.noreply.github.com>
2022-11-13 18:57:05 -0500

    Added Purge Command

--- Butler.py
+++ Butler.py
@@ -33,7 +33,7 @@ async def rickroll(ctx, member=None):
         for user in members:
             if user.id == id:
                 voice = await channel.connect()
-                voice.play(discord.FFmpegPCMAudio(executable=r"C:\Users\kesha\PythonProjects\ffmpeg.exe", source="Rickroll.mp3"))
+                voice.play(discord.FFmpegPCMAudio(executable=r"C:\Users\kesha\Development\PythonProjects\ffmpeg.exe", source="Rickroll.mp3"))
                 return
 
     await ctx.send(f"It seems that user is not in a voice channel right now")
@@ -45,6 +45,13 @@ async def disconnect(ctx):
         return
 
     await ctx.send(f"I am not in a voice channel currently.")
+
+@bot.command(aliases = ['purify'])
+@commands.has_permissions(manage_channels = True)
+async def purge(ctx, amount: int):
+    await ctx.message.delete()
+    await ctx.channel.purge(limit=amount+1)
+
 # This messages prints as soon as the bot has logged on
 @ bot.event
 async def on_ready():
